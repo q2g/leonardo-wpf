508f0fdff4b13e31dc4f982d3100c0f3749b5a15
ChristofHoldschick <Christof.Holdschick@akquinet.de>
2018-03-20 06:37:12 +0100

    +LuiToggleButton

---
+++ src/leonardo-wpf/Controls/LuiToggleButton.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace leonardo.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für LuiToggleButton.xaml
+    /// </summary>
+    public partial class LuiToggleButton : ToggleButton
+    {
+        public LuiToggleButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
