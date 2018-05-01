28ab8716ee5c92fcf9f13f09cac6a93eb0f764a5
ChristofHoldschick <Christof.Holdschick@akquinet.de>
2018-05-01 06:20:12 +0200

    added IndexProvider

---
+++ src/Resources/IIndexProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leonardo.Resources
+{
+    public interface IIndexProvider
+    {
+        void SetIndex(object objectWithIndexProperty, int indexValue);
+        int GetIndex(object objectWithIndexProperty);
+    }
+}
