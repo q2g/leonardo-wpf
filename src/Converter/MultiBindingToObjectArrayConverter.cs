f067968a98f53b9678b77d67d6a48adc55ea03f3
Christof Holdschik <christof.holdschick@akquinet.de>
2018-05-16 16:01:58 +0200

    * Fix show Searchfield on Click

---
+++ src/Converter/MultiBindingToObjectArrayConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace leonardo.Converter
+{
+    #region Usings
+    using NLog;
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    #endregion
+
+    public class MultiBindingToObjectArrayConverter : IMultiValueConverter
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return values.Clone();
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[2];
+        }
+    }
+}
