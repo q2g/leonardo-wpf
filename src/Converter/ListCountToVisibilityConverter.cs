04f5dd216e09bfe241d8ba255b48beecca9cfca5
Christof Holdschik <christof.holdschick@akquinet.de>
2018-07-09 08:33:15 +0200

    Added Converter

---
+++ src/Converter/ListCountToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace leonardo.Converter
+{
+    #region Usings
+    using System;
+    using System.Collections;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    #endregion
+
+    public class ListCountToVisibilityConverter : IValueConverter
+    {
+        public int CountToMatch { get; set; }
+        public Visibility CountMatchesVisibility { get; set; }
+        public Visibility ElseVisibility { get; set; }
+
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ICollection col)
+            {
+                return col.Count == CountToMatch ? this.CountMatchesVisibility : this.ElseVisibility;
+            }
+
+            return ElseVisibility;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
